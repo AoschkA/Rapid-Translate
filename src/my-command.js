71eb9740b7745889fbf072f3ba607ff96a37dc41
Jonas Praëm Andreassen <jpa@impact.dk>
2018-04-12 21:42:48 +0200

    Adding operators and multiple parameter intepretation

--- src/my-command.js
+++ src/my-command.js
@@ -1,33 +1,29 @@
 var sketch = require('sketch');
-var doc = context.document;
-var selection = context.selection;
-//var document = sketch.getSelectedDocument();
-context.document.showMessage('hello');
+
 const selectedLayers = context.selection;
 const selectedCount = selectedLayers.length;
 
 if (selectedCount === 0) {
-    context.document.showMessage('No layers are selected.');
+    context.document.showMessage('Nothing selected..');
 } else {
-
-    var outputString = sketch.UI.getStringFromUser('Type command', 'default');
+    var outputString = sketch.UI.getStringFromUser('Type commando', '');
     interpret(outputString);
 }
 
 function interpret(input) {
     var parameters = input.split(/(?=[a-z])/);
-    var y;
-    var frame;
-    var layer;
     var extraParameters = [];
-    context.document.showMessage(parameters);
     for (var i = 0; i<parameters.length; i++) {
         if (parameters[i].length === 1) {
-            context.document.showMessage('multiple commandX: '+parameters);
             extraParameters.push(parameters[i]);
-
         } else {
-            var value = Number(parameters[i].substring(1));
+            var value;
+            if (isNaN(parameters[i].substring(1))) {
+                var tokens = parameters[i].substring(1).split(/([\+\-\*\/])/);
+                value = operate(tokens[1], Number(tokens[0]), Number(tokens[2]));
+            } else {
+                value = Number(parameters[i].substring(1));
+            }
 
             if (extraParameters.length !== 0) {
                 for (var j = 0; j<extraParameters.length; j++) {
@@ -72,6 +68,25 @@ function translate(toTranselate, value) {
             frame = layer.frame();
             frame.setHeight(value);
         }
+    }
+}
+
+function operate(operator, prefix, suffix) {
+
+    if (operator === '+') {
+        return prefix + suffix;
+    }
+
+    else if (operator === '-') {
+        return prefix - suffix;
+    }
+
+    else if (operator === '*') {
+        return prefix * suffix;
+    }
+
+    else if (operator === '/') {
+        return prefix / suffix;
     }
 }
 
