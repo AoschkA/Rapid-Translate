c232a86d142c5a244f43fb2f6853a8140ec01c44
Jonas Praëm Andreassen <jpa@impact.dk>
2018-04-22 17:31:15 +0200

    Woroking + & - operators

--- src/my-command.js
+++ src/my-command.js
@@ -9,6 +9,12 @@ if (selectedCount === 0) {
     var outputString = sketch.UI.getStringFromUser('Type commando', '');
     interpret(outputString);
 }
+/*
+copiedWidth = Math.round(frame.width());
+	copiedHeight = Math.round(frame.height());
+	copiedX = Math.round(layer.absoluteRect().rulerX());
+    copiedY = Math.round(layer.absoluteRect().rulerY());
+    */
 
 function interpret(input) {
     var parameters = input.split(/(?=[a-z])/);
@@ -20,9 +26,20 @@ function interpret(input) {
             var value;
             if (isNaN(parameters[i].substring(1))) {
                 var tokens = parameters[i].substring(1).split(/([\+\-\*\/])/);
+                context.document.showMessage('tokens: '+tokens);
                 value = operate(tokens[1], Number(tokens[0]), Number(tokens[2]));
             } else {
-                value = Number(parameters[i].substring(1));
+                var valueToAnalyse = parameters[i].substring(1);
+                var operator = valueToAnalyse.charAt(0);
+                if (operator === '+' || operator === '-' || operator === '*' || operator == '/') {
+                    // if starts with an operator
+                    context.document.showMessage('toTranslate: '+parameters[i].charAt(0)+ ' operator: ' +operator+ ' value: '+valueToAnalyse.substring(1));
+                    translateFromOriginalValue(parameters[i].charAt(0), operator, valueToAnalyse.substring(1));
+                    break;
+                } else {
+                    // If it's just a number
+                    value = Number(valueToAnalyse);
+                }
             }
 
             if (extraParameters.length !== 0) {
@@ -39,6 +56,7 @@ function interpret(input) {
 
 function translate(toTranselate, value) {
     toTranselate = toTranselate.toLowerCase();
+    //context.document.showMessage('value: '+value);
 
     if (toTranselate === 'x') {
         for (y = 0; y<context.selection.length; y++) {
@@ -69,6 +87,84 @@ function translate(toTranselate, value) {
             frame.setHeight(value);
         }
     }
+}
+
+function translateFromOriginalValue(toTranslate, operator, value) {
+    value = Number(value);
+    for (y = 0; y<context.selection.length; y++) {
+        context.document.showMessage('function');
+        layer = context.selection[y];
+        frame = layer.frame();
+        // Copying original values
+        copiedW = Math.round(frame.width());
+	    copiedH = Math.round(frame.height());
+	    copiedX = Math.round(layer.absoluteRect().rulerX());
+        copiedY = Math.round(layer.absoluteRect().rulerY());
+        var value;
+
+        if (toTranslate === 'x') {
+            if (operator === '+') {
+                value = Number(copiedX) + value;
+            } else if (operator === '-') {
+                value = Number(copiedX) - value;
+            } else if (operator === '*') {
+                value = Number(copiedX) * value;
+            } else if (operator === '/') {
+                value = Number(copiedX) / value;
+            } else {
+                // Break out if no operator match
+                break;
+            }
+            layer.absoluteRect().setRulerX(value);
+        } else if (toTranslate === 'y') {
+            if (operator === '+') {
+                value = Number(copiedY) + value;
+            } else if (operator === '-') {
+                value = Number(copiedY) - value;
+            } else if (operator === '*') {
+                value = Number(copiedY) * value;
+            } else if (operator === '/') {
+                value = Number(copiedY) / value;
+            } else {
+                // Break out if no operator match
+                break;
+            }
+            layer.absoluteRect().setRulerY(value);
+        } else if (operator === 'w') {
+            if (operator === '+') {
+                value = Number(copiedW) + value;
+            } else if (operator === '-') {
+                value = Number(copiedW) - value;
+            } else if (operator === '*') {
+                value = Number(copiedW) * value;
+            } else if (operator === '/') {
+                value = Number(copiedW) / value;
+            } else {
+                // Break out if no operator match
+                break;
+            }
+            frame.setWidth(value);
+        } else if (operator === 'h') {
+            if (operator === '+') {
+                value = Number(copiedH) + value;
+            } else if (operator === '-') {
+                value = Number(copiedH) - value;
+            } else if (operator === '*') {
+                value = Number(copiedH) * value;
+            } else if (operator === '/') {
+                value = Number(copiedH) / value;
+            } else {
+                // Break out if no operator match
+                break;
+            }
+            frame.setHeight(value);
+        }
+    }
+    context.document.reloadInspector();
+}
+
+function rawTranslate() {
+
 }
 
 function operate(operator, prefix, suffix) {
