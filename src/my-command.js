c9f1963bb4b8459fe6f4b09d4178a7ec759143f8
Jonas Praëm Andreassen <jpa@impact.dk>
2018-04-23 21:52:21 +0200

    Adding RAW tranlations

--- src/my-command.js
+++ src/my-command.js
@@ -16,39 +16,60 @@ copiedWidth = Math.round(frame.width());
     copiedY = Math.round(layer.absoluteRect().rulerY());
     */
 
+    
+/**
+ * Interprets the input - the programs main function
+ * 
+ * RAW translation uses the original value of the element. 
+ * NEW translation sets the value
+ * 
+ * @param {any} input 
+ */
 function interpret(input) {
     var parameters = input.split(/(?=[a-z])/);
     var extraParameters = [];
-    for (var i = 0; i<parameters.length; i++) {
+    var isRaw = false;
+    for (var i = 0; i < parameters.length; i++) {
         if (parameters[i].length === 1) {
             extraParameters.push(parameters[i]);
         } else {
+            var toTranslate = parameters[i].charAt(0);
             var value;
-            if (isNaN(parameters[i].substring(1))) {
+            if (isNaN(parameters[i].substring(1)) || parameters[i].charAt(1) === '+' ||  parameters[i].charAt(1) === '-') {
                 var tokens = parameters[i].substring(1).split(/([\+\-\*\/])/);
-                context.document.showMessage('tokens: '+tokens);
-                value = operate(tokens[1], Number(tokens[0]), Number(tokens[2]));
+                context.document.showMessage('tokens: ' + tokens[1]);
+
+                if (tokens[1] === '+' || tokens[1] === '-' || tokens[1] === '*' || tokens[1] === '/') {
+                    // Raw translation
+                    if (extraParameters.length !== 0) {
+                        for (var j = 0; j < extraParameters.length; j++) {
+                            translateFromOriginalValue(extraParameters[j], tokens[1], tokens[2]);
+                        }
+                    }
+                    translateFromOriginalValue(toTranslate, tokens[1], tokens[2]);
+                    isRaw = true;
+                } else {
+                    // NEW translations
+                    value = operate(tokens[1], Number(tokens[0]), Number(tokens[2]));
+                    isRaw = false;
+                }
             } else {
                 var valueToAnalyse = parameters[i].substring(1);
                 var operator = valueToAnalyse.charAt(0);
-                if (operator === '+' || operator === '-' || operator === '*' || operator == '/') {
-                    // if starts with an operator
-                    context.document.showMessage('toTranslate: '+parameters[i].charAt(0)+ ' operator: ' +operator+ ' value: '+valueToAnalyse.substring(1));
-                    translateFromOriginalValue(parameters[i].charAt(0), operator, valueToAnalyse.substring(1));
-                    break;
-                } else {
-                    // If it's just a number
-                    value = Number(valueToAnalyse);
-                }
-            }
 
-            if (extraParameters.length !== 0) {
-                for (var j = 0; j<extraParameters.length; j++) {
-                    translate(extraParameters[j], value);
-                }
+                // If it's just a number
+                value = Number(valueToAnalyse);
             }
-            var toTranslate = parameters[i].charAt(0);
-            translate(toTranslate, value);
+            if (!isRaw) {
+                // Protect from direct operations
+                if (extraParameters.length !== 0) {
+                    for (var j = 0; j < extraParameters.length; j++) {
+                        translate(extraParameters[j], value);
+                    }
+                }
+                
+                translate(toTranslate, value);
+            }
         }
     }
     context.document.reloadInspector();
@@ -59,29 +80,23 @@ function translate(toTranselate, value) {
     //context.document.showMessage('value: '+value);
 
     if (toTranselate === 'x') {
-        for (y = 0; y<context.selection.length; y++) {
+        for (y = 0; y < context.selection.length; y++) {
             layer = context.selection[y];
             layer.absoluteRect().setRulerX(value);
         }
-    }
-
-    else if (toTranselate === 'y') {
-        for (y = 0; y<context.selection.length; y++) {
+    } else if (toTranselate === 'y') {
+        for (y = 0; y < context.selection.length; y++) {
             layer = context.selection[y];
             layer.absoluteRect().setRulerY(value);
         }
-    }
-
-    else if (toTranselate === 'w') {
-        for (y = 0; y<context.selection.length; y++) {
+    } else if (toTranselate === 'w') {
+        for (y = 0; y < context.selection.length; y++) {
             layer = context.selection[y];
             frame = layer.frame();
             frame.setWidth(value);
         }
-    }
-
-    else if (toTranselate === 'h') {
-        for (y = 0; y<context.selection.length; y++) {
+    } else if (toTranselate === 'h') {
+        for (y = 0; y < context.selection.length; y++) {
             layer = context.selection[y];
             frame = layer.frame();
             frame.setHeight(value);
@@ -91,14 +106,14 @@ function translate(toTranselate, value) {
 
 function translateFromOriginalValue(toTranslate, operator, value) {
     value = Number(value);
-    for (y = 0; y<context.selection.length; y++) {
+    for (y = 0; y < context.selection.length; y++) {
         context.document.showMessage('function');
         layer = context.selection[y];
         frame = layer.frame();
         // Copying original values
         copiedW = Math.round(frame.width());
-	    copiedH = Math.round(frame.height());
-	    copiedX = Math.round(layer.absoluteRect().rulerX());
+        copiedH = Math.round(frame.height());
+        copiedX = Math.round(layer.absoluteRect().rulerX());
         copiedY = Math.round(layer.absoluteRect().rulerY());
         var value;
 
@@ -163,29 +178,15 @@ function translateFromOriginalValue(toTranslate, operator, value) {
     context.document.reloadInspector();
 }
 
-function rawTranslate() {
-
-}
-
 function operate(operator, prefix, suffix) {
 
     if (operator === '+') {
         return prefix + suffix;
-    }
-
-    else if (operator === '-') {
+    } else if (operator === '-') {
         return prefix - suffix;
-    }
-
-    else if (operator === '*') {
+    } else if (operator === '*') {
         return prefix * suffix;
-    }
-
-    else if (operator === '/') {
+    } else if (operator === '/') {
         return prefix / suffix;
     }
 }
-
-
-
-
